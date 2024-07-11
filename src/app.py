3512f944b0a36a66ff2862c753c7bbb422dd1ab5
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-07-10 22:06:08 -0300

    tests: remove setup app from codecov

--- src/app.py
+++ src/app.py
@@ -13,6 +13,6 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':  # pragma: no cover
+    app = create_app()  # pragma: no cover
+    app.run(host='0.0.0.0', port=8080)  # pragma: no cover
