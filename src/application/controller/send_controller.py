8db58774a224b496731e983553d679f0c12e04bb
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-07-12 15:51:14 -0300

    chore: Debug

--- src/application/controller/send_controller.py
+++ src/application/controller/send_controller.py
@@ -2,7 +2,7 @@ from src.application.services.send_api_data import SendService
 from src.adapters.pubsub import PubSub
 
 
-def send(request_body):
-    response = SendService.send(send_repository=PubSub, request=request_body)
+def send(request_body: dict):
+    response = SendService.send(send_repository=PubSub, request_body=request_body)
 
     return response
--- src/application/routes/send.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request
-from src.application.controller.send_controller import send
-
-send_bp = Blueprint('send', __name__)
-
-
-@send_bp.route('/', methods=['POST'])
-def send_route():
-    return send(request)
